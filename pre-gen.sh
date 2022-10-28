9940b179cabda7456ecadf979dfb7617ad1c9a01
Bruno Willenborg <b.willenborg@tum.de>
2022-10-28 10:51:56 +0200

    added backup script

---
+++ pre-gen.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "--- $(date --utc +%FT%TZ) ----------------------------------------" >>$HOME/pre-generate.log 2>&1
+docker exec -i --user 33 nextcloud-app-1 ./occ preview:pre-generate -v >>$HOME/pre-generate.log 2>&1

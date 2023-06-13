82de0dd41480f95786e95cbca90b4b3e5d6487bd
Bruno Willenborg <b.willenborg@tum.de>
2023-06-13 11:49:49 +0200

    less verbose db dump output

--- backup/nextcloud_backup_restic.sh
+++ backup/nextcloud_backup_restic.sh
@@ -50,7 +50,7 @@ docker run -i --rm --name pgdump \
     --network nextcloud_net \
     -e PGPASSWORD="$DB_PASSWORD" \
   postgres:14-alpine \
-    -v -h "$DB_HOST" -U "$DB_USER" \
+    -h "$DB_HOST" -U "$DB_USER" \
     -d "$DB_NAME" -f /data/nextcloud-sqlbkp.bak \
     >> ${LOGFILE} 2>&1
 

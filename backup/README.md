89c3874a9dd066e550a373077d6d22789c8d2c52
Bruno Willenborg <b.willenborg@tum.de>
2023-06-13 10:30:27 +0200

    rc1

--- backup/README.md
+++ backup/README.md
@@ -14,13 +14,29 @@ Add this to `sudo crontab -e`. Root is required!
 ## Env
 
 ```shell
-NEXTCLOUD_BACKUP_PATHS="/media/samsung/nextcloud/data/data/ /media/samsung/nextcloud/data/config/ /media/samsung/nextcloud/data/themes/"
+###############################################################################
+# Nextcloud restic backup settings
+###############################################################################
 
+# General settings
+NEXTCLOUD_BACKUP_LOGFILE="$HOME/nextcloud-backup-restic.log"
+NEXTCLOUD_BACKUPDIR_TEMP="/tmp/nextcloud/backup"
+
+# Restic settings
+NEXTCLOUD_RESTIC_INCLUDE_FILE="/nextcloud/backup/include.txt"
+NEXTCLOUD_RESTIC_EXCLUDE_FILE="/nextcloud/backup/exclude.txt"
+NEXTCLOUD_RESTIC_PASSWORD="changeMe"
+NEXTCLOUD_RESTIC_FORGET_POLICY="--keep-within-daily 56d --keep-within-weekly 6m --keep-within-monthly 1y --keep-within-yearly 5y"
 NEXTCLOUD_RESTIC_REPO_LOCAL="/media/intenso/restic/nextcloud"
 NEXTCLOUD_RESTIC_REPO_AZURE="azure:restic:/nextcloud"
-NEXTCLOUD_RESTIC_PASSWORD="changeMe"
+
+# Azure account name and key
 NEXTCLOUD_AZURE_ACCOUNT_NAME="accountname"
 NEXTCLOUD_AZURE_ACCOUNT_KEY="changeMe"
 
-
+# Nextcloud database credentials
+NEXTCLOUD_DB_HOST=db
+NEXTCLOUD_DB_NAME=nextcloud
+NEXTCLOUD_DB_USER=nextcloud
+NEXTCLOUD_DB_PASSWORD=changeMe
 ```
--- backup/nextcloud_backup_restic.sh
+++ backup/nextcloud_backup_restic.sh
@@ -4,36 +4,37 @@
 # 0 4 * * * $HOME/myzsh/tools/nextcloud/nextcloud-backup.sh >/dev/null 2>&1
 
 # config ----------------------------------------------------------------------
-# Nextcloud data, config and themees path, space separated
-NEXTCLOUD_BACKUP_PATHS="/my/nextcloud/data/ /my/nextcloud/config/ /my/nextcloud/themes/"
-# Log file path
-LOGFILE="/home/me/nextcloud.log"
-# Temp dir for DB dump
-BACKUPDIR_DB_TEMP=/tmp/nextcloud_backup/db
-ERR=0
+# General settings
+LOGFILE="${NEXTCLOUD_BACKUP_LOGFILE:-$HOME/nextcloud-backup-restic.log}"
+BACKUPDIR_DB_TEMP="${NEXTCLOUD_BACKUPDIR_TEMP:-/tmp/nextcloud/backup}"
 
-# Restic repo password
+# Restic setings
+INCLUDE_FILE="${NEXTCLOUD_RESTIC_INCLUDE_FILE:-/nextcloud/backup/include.txt}"
+EXCLUDE_FILE="${NEXTCLOUD_RESTIC_EXCLUDE_FILE:-/nextcloud/backup/exclude.txt}"
 export RESTIC_PASSWORD="${NEXTCLOUD_RESTIC_PASSWORD:-changeMe}"
-RESTIC_REPOSITORY_LOCAL="/media/myhdd/restic/nextcloud"
-RESTIC_REPOSITORY_AZURE="azure:restic:/nextcloud"
+FORGET_POLICY="${NEXTCLOUD_RESTIC_FORGET_POLICY:---keep-within-daily 7d --keep-within-weekly 6m}"
+RESTIC_REPOSITORY_LOCAL="${NEXTCLOUD_RESTIC_REPO_LOCAL:-/media/myhdd/restic/nextcloud}"
+RESTIC_REPOSITORY_AZURE="${NEXTCLOUD_RESTIC_REPO_AZURE:-azure:restic:/nextcloud}"
 
 # Azure Storage Account name and key
 export AZURE_ACCOUNT_NAME="${NEXTCLOUD_AZURE_ACCOUNT_NAME:-accountname}"
 export AZURE_ACCOUNT_KEY="${NEXTCLOUD_AZURE_ACCOUNT_KEY:-changeMe}"
 
-
 # Nextcloud database
-NEXTCLOUD_DB_HOST=db
-NEXTCLOUD_DB_NAME=nextcloud
-NEXTCLOUD_DB_USER=nextcloud
-NEXTCLOUD_DB_PASSWORD=changeMe
+NEXTCLOUD_DB_HOST="${NEXTCLOUD_DB_HOST:-db}"
+NEXTCLOUD_DB_NAME="${NEXTCLOUD_DB_NAME:-nextcloud}"
+NEXTCLOUD_DB_USER="${NEXTCLOUD_DB_USER:-nextcloud}"
+NEXTCLOUD_DB_PASSWORD="${NEXTCLOUD_DB_PASSWORD:-changeMe}"
 
 # script ----------------------------------------------------------------------
+ERR=0
 printf "\n\n" >> ${LOGFILE}
-echo "-- Nextcloud backup " `date --utc +%FT%TZ` "-----------------------------" >> ${LOGFILE}
+echo "-- Nextcloud backup " `date --utc +%FT%TZ` "-----------------------------" \
+  >> ${LOGFILE}
 
 # Enable maintenance mode
-docker exec -i --user 33 nextcloud-app-1 ./occ maintenance:mode --on >> ${LOGFILE} 2>&1
+docker exec -i --user 33 nextcloud-app-1 ./occ maintenance:mode --on \
+  >> ${LOGFILE} 2>&1
 
 errtmp=$?
 ERR=$(($ERR + $errtmp))
@@ -48,7 +49,9 @@ docker run -i --rm --name pgdump \
     --network nextcloud_net \
     -e PGPASSWORD="$NEXTCLOUD_DB_PASSWORD" \
   postgres:14-alpine \
-    -v -h "$NEXTCLOUD_DB_HOST" -U "$NEXTCLOUD_DB_USER" -d "NEXTCLOUD_DB_NAME" -f /data/nextcloud-sqlbkp.bak >> ${LOGFILE} 2>&1
+    -v -h "$NEXTCLOUD_DB_HOST" -U "$NEXTCLOUD_DB_USER" \
+    -d "NEXTCLOUD_DB_NAME" -f /data/nextcloud-sqlbkp.bak \
+    >> ${LOGFILE} 2>&1
 
 errtmp=$?
 ERR=$(($ERR + $errtmp))
@@ -56,20 +59,20 @@ echo "dump nextcloud db " $errtmp >> ${LOGFILE}
 
 # Create testic snapshot - local
 # Local restic repo
-export RESTIC_REPOSITORY="${NEXTCLOUD_RESTIC_REPO_LOCAL:-/media/intenso/restic/nextcloud}"
-echo "$BACKUPDIR_DB_TEMP $NEXTCLOUD_BACKUP_PATHS" | \
-  xargs \
-  restic backup --no-scan >> ${LOGFILE} 2>&1
+export RESTIC_REPOSITORY="${NEXTCLOUD_RESTIC_REPO_LOCAL}"
+restic backup --no-scan \
+  --files-from "${INCLUDE_FILE}" \
+  --iexclude-file "${EXCLUDE_FILE}" >> ${LOGFILE} 2>&1
 
 errtmp=$?
 ERR=$(($ERR + $errtmp))
 echo "create restic snapshot - local" $errtmp >> ${LOGFILE}
 
 # Create restic snapshot - Azure
-export RESTIC_REPOSITORY="${NEXTCLOUD_RESTIC_REPO_AZURE:-azure:restic:/nextcloud}"
-echo "$BACKUPDIR_DB_TEMP $NEXTCLOUD_BACKUP_PATHS" | \
-  xargs \
-  restic backup --no-scan >> ${LOGFILE} 2>&1
+export RESTIC_REPOSITORY="${NEXTCLOUD_RESTIC_REPO_AZURE}"
+restic backup --no-scan \
+  --files-from "${INCLUDE_FILE}" \
+  --iexclude-file "${EXCLUDE_FILE}" >> ${LOGFILE} 2>&1
 
 errtmp=$?
 ERR=$(($ERR + $errtmp))
@@ -92,19 +95,16 @@ echo "cleanup backup dir " $errtmp >> ${LOGFILE}
 
 # Cleanup restic repos
 # local
-export RESTIC_REPOSITORY="${NEXTCLOUD_RESTIC_REPO_LOCAL:-/media/intenso/restic/nextcloud}"
-restic forget --prune \
-  --keep-within-daily 56d --keep-within-weekly 6m --keep-within-monthly 1y --keep-within-yearly 5y \
-  >> ${LOGFILE} 2>&1
+export RESTIC_REPOSITORY="${NEXTCLOUD_RESTIC_REPO_LOCAL}"
+restic forget --prune $FORGET_POLICY >> ${LOGFILE} 2>&1
 
 errtmp=$?
 ERR=$(($ERR + $errtmp))
 echo "cleanup restic snapshots - local " $errtmp >> ${LOGFILE}
 
 # Azure
-export RESTIC_REPOSITORY="${NEXTCLOUD_RESTIC_REPO_AZURE:-azure:restic:/nextcloud}"
-restic forget --prune \
-  --keep-within-daily 56d --keep-within-weekly 6m --keep-within-monthly 1y --keep-within-yearly 5y \
+export RESTIC_REPOSITORY="${NEXTCLOUD_RESTIC_REPO_AZURE}"
+restic forget --prune restic forget --prune $FORGET_POLICY >> ${LOGFILE} 2>&1
   >> ${LOGFILE} 2>&1
 
 errtmp=$?
